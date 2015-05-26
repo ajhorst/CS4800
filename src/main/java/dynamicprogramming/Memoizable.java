e1e0a25d6730b114f7a9b194f1547bd53abbfa5d
AJ Horst <ajhorst@gmail.com>
2015-05-26 17:09:22 -0400

    create abstract class for memoization

---
+++ src/main/java/dynamicprogramming/Memoizable.java
@@ -0,0 +1,19 @@
+package dynamicprogramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Memoizable<X, Y> {
+	
+	protected Map<X, Y> memo = new HashMap<X, Y>();
+	
+	public Y get(X x){
+		if(!memo.containsKey(x)){
+			memo.put(x, compute(x));
+		}
+		return memo.get(x);
+		
+	}
+	
+	public abstract Y compute(X x);
+}
---
+++ src/test/java/dynamicprogramming/TestMemoizable.java
@@ -0,0 +1,25 @@
+package dynamicprogramming;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestMemoizable {
+
+	@Test
+	public void testMemoizable(){
+		Memoizable<Integer, Integer> m = new Memoizable<Integer, Integer>(){
+			@Override
+			public Integer compute(Integer x) {
+				return x;
+		}};
+		
+		assertEquals("Memoizable map should be empty", 0, m.memo.size());
+		m.get(0);
+		assertEquals("Memoizable map should be size 1", 1, m.memo.size());
+		m.get(0);
+		assertEquals("Memoizable map should still be size 1", 1, m.memo.size());
+		m.get(1);
+		assertEquals("Memoizable map should now be size 2", 2, m.memo.size());
+	}
+}
