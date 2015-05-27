16cfb4fb5716ec306a68e72d8daf491440aaded5
AJ Horst <ajhorst@gmail.com>
2015-05-27 17:05:01 -0400

    Finish knapsack algorithm
    - algo avoids recomputing results thanks to the MemoizerPair extension
    - result list branch2 is safely mutable thanks to the new cloning feature of the memoizer classes
    - implement deep equality for Item, because the algo won't return the same references of the items due to cloning

--- src/main/java/dynamicprogramming/knapsack/Knapsack.java
+++ src/main/java/dynamicprogramming/knapsack/Knapsack.java
@@ -1,8 +1,39 @@
 package dynamicprogramming.knapsack;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+import dynamicprogramming.memoizable.MemoizerPair;
+
 public class Knapsack extends MemoizerPair<Integer, Integer, List<Item>>{
+
+	private final Item[] items;
+	private final int maxCapacity;
+	
+	public Knapsack(int maxCapacity, Item... items){
+		this.maxCapacity = maxCapacity;
+		this.items = items;
+	}
+	
+	public List<Item> solve(){
+		return this.get(items.length -1, this.maxCapacity);
+	}
+	
+	@Override
+	public List<Item> compute(Integer index, Integer capacity) {
+		if(index < 0){
+			return new ArrayList<Item>(); // empty 
+		}
+		Item currentItem = items[index];
+		if(capacity < currentItem.weight){ // not enough room, skip this item
+			return this.get(index-1, capacity);
+		}
+		List<Item> branch1 = this.get(index -1, capacity); // outcome of skipping this item
+		List<Item> branch2 = this.get(index -1, capacity - currentItem.weight);
+		branch2.add(currentItem); // outcome of including this item
+		return sumValue(branch1) > sumValue(branch2) ? branch1 : branch2;
+	}
 	
 	protected static int sumValue(List<Item> items, Item... extraItems){
 		int result = 0;
@@ -20,5 +51,13 @@ class Item{
 	public Item(Integer value, Integer weight){
 		this.value = value;
 		this.weight = weight;
+	}
+	
+	public boolean equals(Object obj){
+		if (!(obj instanceof Item)){
+			return false;
+		}
+		Item other = (Item)obj;
+		return this.value == other.value && this.weight == other.weight;
 	}
 }
--- src/test/java/dynamicprogramming/knapsack/TestKnapsack.java
+++ src/test/java/dynamicprogramming/knapsack/TestKnapsack.java
@@ -1,6 +1,7 @@
 package dynamicprogramming.knapsack;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
@@ -11,17 +12,41 @@ import com.google.common.collect.ImmutableList;
 
 public class TestKnapsack {
 
-	private static Item i1, i2;
+	private static Item i1, i2, i3, i4, i5;
 	
 	@BeforeClass
 	public static void setup(){
 		i1 = new Item(1, 1);
 		i2 = new Item(6, 2);
+		i3 = new Item(18, 5);
+		i4 = new Item(22, 6);
+		i5 = new Item(28, 7);
 	}
 	
 	@Test
 	public void testSumList(){
 		List<Item> l = ImmutableList.of(i1, i2);
 		assertEquals("values should sum to 7", 7, Knapsack.sumValue(l));
+	}
+	
+	@Test
+	public void testCompute(){
+		List<Item> emptyResult = new Knapsack(0, i1, i2, i3, i4, i5).solve();
+		assertEquals("Solving on an empty list should result in an empty list", 0, emptyResult.size());
+		
+		List<Item> smallCapacityResult = new Knapsack(1, i1, i2).solve();
+		assertEquals("Result should only have one element", 1, smallCapacityResult.size());
+		assertEquals("Result should contain item 1", i1, smallCapacityResult.get(0));
+		
+		List<Item> oneItemResult = new Knapsack(2, i1, i2).solve();
+		assertEquals("Result should only have one element", 1, oneItemResult.size());
+		assertEquals("Result should contain item 2", i2, oneItemResult.get(0));
+		
+		List<Item> generalResult = new Knapsack(8, i1, i2, i3, i4, i5).solve();
+		assertEquals("Result should have two elements but was" + generalResult, 2, generalResult.size());
+		assertTrue("Result should contain item 5", generalResult.contains(i5));
+		assertTrue("Result should contain item 1", generalResult.contains(i1));
+		
+		
 	}
 }
