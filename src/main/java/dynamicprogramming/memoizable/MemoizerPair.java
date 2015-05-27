75b697fdaceabe9f5d6de7d94bf034d8820e9606
AJ Horst <ajhorst@gmail.com>
2015-05-27 01:05:43 -0400

    change implementation of two memoizer classes to use composition over inheritance

--- src/main/java/dynamicprogramming/memoizable/MemoizerPair.java
+++ src/main/java/dynamicprogramming/memoizable/MemoizerPair.java
@@ -4,17 +4,22 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 
-public abstract class MemoizerPair<X, Y, Z> extends Memoizer<Pair<X, Y>, Z> {
+public abstract class MemoizerPair<X, Y, Z>{
+
+	protected final Memoizer<Pair<X, Y>, Z> m;
 	
+	public MemoizerPair(){
+		final MemoizerPair<X, Y, Z> pair = this;
+		m = new Memoizer<Pair<X, Y>, Z>(){
+			public Z compute(Pair<X, Y> xy) {
+				return pair.compute(xy.getKey(), xy.getValue());
+		}};
+	}
+	
+	public Z get(X x, Y y) {
+		return m.get(new ImmutablePair<X, Y>(x, y));
+	}
+
 	public abstract Z compute(X x, Y y);
 	
-	public Z get(X x, Y y){
-		Pair<X, Y> p = new ImmutablePair<>(x, y);
-		return super.get(p);
-	}
-	
-	@Override
-	public Z compute(Pair<X, Y> p){
-		return compute(p.getKey(), p.getValue());
-	}
 }
--- src/test/java/dynamicprogramming/memoizable/TestMemoizer.java
+++ src/test/java/dynamicprogramming/memoizable/TestMemoizer.java
@@ -25,19 +25,19 @@ public class TestMemoizer {
 	
 	@Test
 	public void testMemoizablePair(){
-		MemoizerPair<Integer, Integer, Integer> m = new MemoizerPair<Integer, Integer, Integer>(){
+		MemoizerPair<Integer, Integer, Integer> mPair = new MemoizerPair<Integer, Integer, Integer>(){
 			@Override
 			public Integer compute(Integer x, Integer y) {
 				return x + y;
 		}};
 
 		
-		assertEquals("Memoizable map should be empty", 0, m.memo.size());
-		m.get(0, 0);
-		assertEquals("Memoizable map should be size 1", 1, m.memo.size());
-		m.get(0, 0);
-		assertEquals("Memoizable map should still be size 1", 1, m.memo.size());
-		m.get(1, 0);
-		assertEquals("Memoizable map should now be size 2", 2, m.memo.size());
+		assertEquals("Memoizable map should be empty", 0, mPair.m.memo.size());
+		mPair.get(0, 0);
+		assertEquals("Memoizable map should be size 1", 1, mPair.m.memo.size());
+		mPair.get(0, 0);
+		assertEquals("Memoizable map should still be size 1", 1, mPair.m.memo.size());
+		mPair.get(1, 0);
+		assertEquals("Memoizable map should now be size 2", 2, mPair.m.memo.size());
 	}
 }
