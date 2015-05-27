045811d9aca4da504ffc3bfc9de3994b90047c43
AJ Horst <ajhorst@gmail.com>
2015-05-27 16:57:58 -0400

    fix issue with memoizers where results could be corrupted though access to a memoized reference
    - test case demonstrates this property with a function that should only ever return an empty list. If the list is added to, it means that the memoized value was modified post-memoization

--- src/main/java/dynamicprogramming/memoizable/Memoizer.java
+++ src/main/java/dynamicprogramming/memoizable/Memoizer.java
@@ -3,15 +3,18 @@ package dynamicprogramming.memoizable;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.rits.cloning.Cloner;
+
 public abstract class Memoizer<X, Y>{
 	
 	protected Map<X, Y> memo = new HashMap<X, Y>();
+	private final Cloner cloner = new Cloner();
 	
 	public Y get(X x){
 		if(!memo.containsKey(x)){
 			memo.put(x, compute(x));
 		}
-		return memo.get(x);
+		return cloner.deepClone(memo.get(x));
 		
 	}
 	
--- src/test/java/dynamicprogramming/memoizable/TestMemoizer.java
+++ src/test/java/dynamicprogramming/memoizable/TestMemoizer.java
@@ -2,7 +2,11 @@ package dynamicprogramming.memoizable;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 public class TestMemoizer {
 
@@ -21,6 +25,23 @@ public class TestMemoizer {
 		assertEquals("Memoizable map should still be size 1", 1, m.memo.size());
 		m.get(1);
 		assertEquals("Memoizable map should now be size 2", 2, m.memo.size());
+	}
+	
+	@Test
+	public void testCloning(){
+		Memoizer<Integer, List<Integer>> m = new Memoizer<Integer, List<Integer>>(){
+			public List<Integer> compute(Integer x) {
+				if(x == 0){
+					return Lists.newLinkedList();
+				}
+				List<Integer> result = this.get(x-1);
+				result.add(0);
+				return this.get(x-1);
+			}
+		};
+		
+		List<Integer> emptyResult = m.get(1);
+		assertEquals("result should always be empty", 0, emptyResult.size());
 	}
 	
 	@Test
