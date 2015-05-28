e8821b40aa7c49ecead95c29512a87e2ed97ec8c
AJ Horst <ajhorst@gmail.com>
2015-05-27 22:36:43 -0400

    create class for Prim minimun spanning tree algorithm

---
+++ src/main/java/minimumspanningtree/Prim.java
@@ -0,0 +1,16 @@
+package minimumspanningtree;
+
+import graph.Edge;
+import graph.Node;
+
+import java.util.Set;
+
+public class Prim implements MinimumSpanningTree{
+
+	@Override
+	public Set<Edge> findMinSpanningTree(Set<Node> graph) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
